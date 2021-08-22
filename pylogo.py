bc50d0855b61646a4bae7705cdd2bd616b5a976a
Rajarshi Banerjee <rajarshiban13@gmail.com>
2021-08-22 18:01:48 +0530

    pylogo

--- pylogo.py
+++ pylogo.py
@@ -7,31 +7,160 @@ class logo:
 
     def neel(i):
         i.t.penup()
+       
+       # d = 200
+       # x = 10
         i.t.pencolor('blue')
-        # i.t.goto(-100,0)
+        i.t.color('blue')
+        i.t.goto(-110,-100)
         i.t.pendown()
+        i.t.begin_fill()
+        i.t.right(180)
+        i.t.forward(100/3)
+
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+        i.t.forward(15)
+        i.t.right(5)
+        for _ in range(5):
+            i.t.forward(15)
+            i.t.right(5)
+        i.t.forward(27.58789)
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+       
+        
+ 
+        i.t.goto(0,100)
+        i.t.goto(0,110)
+        i.t.goto(-100,110)
+        i.t.goto(-100,110+100/3)
         i.t.left(90)
-        i.t.forward(100)
-        i.t.right(90)
-        i.t.forward(50)
-        i.t.right(90)
-        i.t.forward(100)
-        i.t.right(90)
-        i.t.forward(50)
-        i.t.right(90)
-        i.t.forward(50)
-        pass
+      
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+        i.t.forward(15)
+        i.t.right(5)
+        for _ in range(5):
+            i.t.forward(15)
+            i.t.right(5)
+        i.t.forward(27.58789)
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+
+        i.t.forward(60+10)
+
+
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+
+        i.t.right(5)
+        i.t.goto(-100+30,10)
+
+        
+        for _ in range(5):
+            i.t.left(15)
+            i.t.forward(15)
+        i.t.left(5)
+
+        i.t.goto(-110,-100)
+        i.t.end_fill()
 
     def holud(i):
         i.t.penup()
         i.t.pencolor('yellow')
-        i.t.goto(-100,0)
+        i.t.color('yellow')
+        i.t.goto(110,100)
+        i.t.right(90)
+        i.t.pendown()
+        i.t.begin_fill()
+        i.t.right(180)
+        i.t.forward(100/3)
+
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+        i.t.forward(15)
+        i.t.right(5)
+        for _ in range(5):
+            i.t.forward(15)
+            i.t.right(5)
+        i.t.forward(27.58789)
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+       
         
-        pass
+ 
+        i.t.goto(0,-100)
+        i.t.goto(0,-110)
+        i.t.goto(100,-110)
+        i.t.goto(100,-110-100/3)
+        i.t.left(90)
+      
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+        i.t.forward(15)
+        i.t.right(5)
+        for _ in range(5):
+            i.t.forward(15)
+            i.t.right(5)
+        i.t.forward(27.58789)
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+
+        i.t.forward(60+10)
+
+
+        for _ in range(5):
+            i.t.right(15)
+            i.t.forward(15)
+
+        i.t.right(5)
+        i.t.goto(70,-10)
+
+        
+        for _ in range(5):
+            i.t.left(15)
+            i.t.forward(15)
+        i.t.left(5)
+
+        i.t.goto(110,100)
+        i.t.end_fill()
+        
+        
+    def eyes(i):
+        i.t.penup()
+        i.t.color('white')
+        i.t.goto(-70,130)
+        i.t.pendown()
+        i.t.begin_fill()
+        i.t.circle(10)
+        i.t.end_fill()
+
+        i.t.penup()
+        i.t.color('white')
+        i.t.goto(70+20,-130)
+        i.t.pendown()
+        i.t.begin_fill()
+        i.t.circle(10)
+        i.t.end_fill()
+
+        i.t.hideturtle()
+        
+
 
 if __name__ =="__main__":
     t.Turtle()
     obj=logo(t)
     obj.neel()
     obj.holud()
+    obj.eyes()
     t.done()
